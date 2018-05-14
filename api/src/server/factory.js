3410f9e28edca6b7228ddf96f9d135baed82cd64
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2018-05-14 14:40:07 -0300

    CORS config is ready

--- api/src/server/factory.js
+++ api/src/server/factory.js
@@ -1,8 +1,13 @@
 const restify = require('restify')
 const routes = require('../routes')
+const cors = require('./cors')
 
 const factoryServer = () => {
   const server = restify.createServer()
+
+  server.pre(cors.preflight)
+  server.use(cors.actual)
+
   routes(server)
 
   return server
